e7c36ea587c3da87120324160d3373f0f79e95b1
Dinesh Kumawat <dkumawat7627@gmail.com>
2026-01-02 15:22:27 +0530

    Initial commit

---
+++ src/main/java/com/crypto/aes/JavaCryptoAesPocApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.aes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCryptoAesPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCryptoAesPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto/aes/JavaCryptoAesPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.aes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCryptoAesPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
